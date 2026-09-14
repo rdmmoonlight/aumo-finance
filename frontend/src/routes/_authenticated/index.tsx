4c9c0c57b67582f9e782b0d61276dca34f45f090
Obscuron <ndopoer@gmail.com>
2026-09-14 09:44:31 +0700

    fix: clean up local index.tsx after push

--- frontend/src/routes/_authenticated/index.tsx
+++ frontend/src/routes/_authenticated/index.tsx
@@ -4,8 +4,4 @@ import HomePage from '@/pages/HomePage'
 export const Route = createFileRoute('/_authenticated/')({
   ssr: false,
   component: HomePage,
-<<<<<<< HEAD
 })
-=======
-})
->>>>>>> 8e520521fd1d8573bbdb1544b801bbb3baeabb80
