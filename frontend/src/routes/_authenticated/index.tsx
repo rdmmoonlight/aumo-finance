c60e1ae40105c47f7ced530a8c4ea70b5789a8ce
Obscuron <ndopoer@gmail.com>
2026-09-14 09:01:50 +0700

    Update index.tsx

--- frontend/src/routes/_authenticated/index.tsx
+++ frontend/src/routes/_authenticated/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute, redirect } from '@tanstack/react-router'
-import HomePage from '../pages/HomePage'
+import HomePage from '@/pages/HomePage'
 
 export const Route = createFileRoute('/')({
   // Lempar otomatis ke /homepage jika route '/' hanya alias
