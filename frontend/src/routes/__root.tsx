138c5abadd5702ec6d8732713e0df1b10bfed0e5
Obscuron <ndopoer@gmail.com>
2026-09-15 09:59:49 +0700

    Update favicon link from PNG to ICO format

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -42,7 +42,7 @@ export const Route = createRootRoute({
     ],
     links: [
       { rel: 'stylesheet', href: appCss },
-      { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
     ],
   }),
   component: RootComponent,
