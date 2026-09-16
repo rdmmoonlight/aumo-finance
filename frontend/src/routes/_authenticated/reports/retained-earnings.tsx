800d7cdd276a0aae656b51ce9d71db58a7246f39
Obscuron <ndopoer@gmail.com>
2026-09-17 06:28:42 +0700

    Add RetainedEarningsPage route

--- frontend/src/routes/_authenticated/reports/retained-earnings.tsx
+++ frontend/src/routes/_authenticated/reports/retained-earnings.tsx
@@ -1,3 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router'
 import RetainedEarningsPage from '@/pages/reports/RetainedEarningsPage'
+
 export const Route = createFileRoute('/_authenticated/reports/retained-earnings')({ component: RetainedEarningsPage })
