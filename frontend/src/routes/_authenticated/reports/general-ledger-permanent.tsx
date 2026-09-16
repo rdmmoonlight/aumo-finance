2874c8c6fcfcc4f4de6ec17dc822ccbb3a7c403e
Obscuron <ndopoer@gmail.com>
2026-09-17 06:26:31 +0700

    Add route for General Ledger Permanent page

--- frontend/src/routes/_authenticated/reports/general-ledger-permanent.tsx
+++ frontend/src/routes/_authenticated/reports/general-ledger-permanent.tsx
@@ -1,3 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router'
 import GeneralLedgerPermanentPage from '@/pages/reports/GeneralLedgerPermanentPage'
+
 export const Route = createFileRoute('/_authenticated/reports/general-ledger-permanent')({ component: GeneralLedgerPermanentPage })
