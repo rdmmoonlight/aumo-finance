45ee8f9c9d9441f58fb83be0380d4871a7e75aec
Obscuron <ndopoer@gmail.com>
2026-09-17 06:22:50 +0700

    Add route for authenticated tools page

--- frontend/src/routes/_authenticated/tools.tsx
+++ frontend/src/routes/_authenticated/tools.tsx
@@ -1,3 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router'
 import ToolsPage from '@/pages/ToolsPage'
+
 export const Route = createFileRoute('/_authenticated/tools')({ component: ToolsPage })
