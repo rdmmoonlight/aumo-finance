a1d94d05638e6cb6a9c983b7bd192105a34a02f4
Obscuron <ndopoer@gmail.com>
2026-09-17 06:20:23 +0700

    Add route for GuardianPage in authenticated section

--- frontend/src/routes/_authenticated/guardian.tsx
+++ frontend/src/routes/_authenticated/guardian.tsx
@@ -1,3 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router'
 import GuardianPage from '@/pages/GuardianPage'
+
 export const Route = createFileRoute('/_authenticated/guardian')({ component: GuardianPage })
