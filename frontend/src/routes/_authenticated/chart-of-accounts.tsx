e34ec34479609e46edbbfa2af5151b8c26bf2212
Obscuron <ndopoer@gmail.com>
2026-09-17 06:19:24 +0700

    Add ChartOfAccountsPage route

--- frontend/src/routes/_authenticated/chart-of-accounts.tsx
+++ frontend/src/routes/_authenticated/chart-of-accounts.tsx
@@ -1,3 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router'
 import ChartOfAccountsPage from '@/pages/ChartOfAccountsPage'
+
 export const Route = createFileRoute('/_authenticated/chart-of-accounts')({ component: ChartOfAccountsPage })
