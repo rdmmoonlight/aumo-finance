2c9d79cf6e9ba7cae28a84df9a567b5b29e49124
Obscuron <ndopoer@gmail.com>
2026-09-17 06:31:51 +0700

    Add route for authenticated worksheet page

--- frontend/src/routes/_authenticated/reports/worksheet.tsx
+++ frontend/src/routes/_authenticated/reports/worksheet.tsx
@@ -1,3 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router'
 import WorksheetPage from '@/pages/reports/WorksheetPage'
+
 export const Route = createFileRoute('/_authenticated/reports/worksheet')({ component: WorksheetPage })
