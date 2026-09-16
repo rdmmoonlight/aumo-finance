0cd38437f10a2ab56d1c15a42e8c201014227b4e
Obscuron <ndopoer@gmail.com>
2026-09-17 06:27:01 +0700

    Add route for General Ledger Temporary page

--- frontend/src/routes/_authenticated/reports/general-ledger-temporary.tsx
+++ frontend/src/routes/_authenticated/reports/general-ledger-temporary.tsx
@@ -1,3 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router'
 import GeneralLedgerTemporaryPage from '@/pages/reports/GeneralLedgerTemporaryPage'
+
 export const Route = createFileRoute('/_authenticated/reports/general-ledger-temporary')({ component: GeneralLedgerTemporaryPage })
