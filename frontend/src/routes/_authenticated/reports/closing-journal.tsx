8dd86a337abaa74f65d7d8855a728088c5b45d3d
Obscuron <ndopoer@gmail.com>
2026-09-17 06:25:36 +0700

    Add route for Closing Journal page

--- frontend/src/routes/_authenticated/reports/closing-journal.tsx
+++ frontend/src/routes/_authenticated/reports/closing-journal.tsx
@@ -1,3 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router'
 import ClosingJournalPage from '@/pages/reports/ClosingJournalPage'
+
 export const Route = createFileRoute('/_authenticated/reports/closing-journal')({ component: ClosingJournalPage })
