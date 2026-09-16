cc2d2b23a1a61819d482f88a77b4920441e5dab6
Obscuron <ndopoer@gmail.com>
2026-09-17 06:26:04 +0700

    Add route for General Journal page

--- frontend/src/routes/_authenticated/reports/general-journal.tsx
+++ frontend/src/routes/_authenticated/reports/general-journal.tsx
@@ -1,3 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router'
 import GeneralJournalPage from '@/pages/reports/GeneralJournalPage'
+
 export const Route = createFileRoute('/_authenticated/reports/general-journal')({ component: GeneralJournalPage })
