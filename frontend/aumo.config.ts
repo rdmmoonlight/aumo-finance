bf79818e0e5f9855c6456886484624df1322d79a
Obscuron <ndopoer@gmail.com>
2026-09-17 06:04:05 +0700

    Clean up comments in aumo.config.ts
    Removed comments from aumoConfig in aumo.config.ts.

--- frontend/aumo.config.ts
+++ frontend/aumo.config.ts
@@ -1,16 +1,13 @@
 import path from 'node:path'
 
 export const aumoConfig = {
-  // 1. Environment Prefix & Target Backend
   envPrefix: ['WEB_'],
   backendTarget: process.env.WEB_API_URL || 'http://localhost:5000',
 
-  // 2. Path Alias Setup
   alias: {
     '@': path.resolve(process.cwd(), './src'),
   },
 
-  // 3. Rules Rewrite / Proxy API
   getRewrites: () => {
     const target = process.env.WEB_API_URL || 'http://localhost:5000'
     return [
