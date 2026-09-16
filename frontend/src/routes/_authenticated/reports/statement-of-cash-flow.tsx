2d8dbf33538811ea077994b9473b9a7aaad4d820
Obscuron <ndopoer@gmail.com>
2026-09-17 06:30:27 +0700

    Add route for Statement of Cash Flow page

--- frontend/src/routes/_authenticated/reports/statement-of-cash-flow.tsx
+++ frontend/src/routes/_authenticated/reports/statement-of-cash-flow.tsx
@@ -1,3 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router'
 import StatementOfCashFlowPage from '@/pages/reports/StatementOfCashFlowPage'
+
 export const Route = createFileRoute('/_authenticated/reports/statement-of-cash-flow')({ component: StatementOfCashFlowPage })
