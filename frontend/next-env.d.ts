98e5198b22752c5307d8cf8b23100f32e7fb6f6a
Obscuron <ndopoer@gmail.com>
2026-09-17 09:00:09 +0700

    Declare module for CSS files
    Add TypeScript declaration for CSS modules.

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -3,3 +3,7 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
+declare module '*.css' {
+  const content: Record<string, string>
+  export default content
+}
