11cd8730556902bf7e850b2df3d43f0f3b268726
Obscuron <ndopoer@gmail.com>
2026-09-17 10:15:49 +0700

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,9 +3,6 @@ import aumoConfig from './aumo.config.ts'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Hanya proses file yang secara eksplisit berada di folder app
-  pageExtensions: ['app.tsx', 'app.ts', 'app.jsx', 'app.js'],
-
   eslint: {
     ignoreDuringBuilds: true,
   },
