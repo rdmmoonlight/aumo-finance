08ead0c6d0d15beb5a458d86aba408b21e861511
Obscuron <ndopoer@gmail.com>
2026-09-22 14:44:33 +0700

    Update GlobalUsings.cs

--- blazor-legacy/GlobalUsings.cs
+++ blazor-legacy/GlobalUsings.cs
@@ -1,9 +1,9 @@
-// 1. Alias Namespace (Sintaks: global using ALIAS = TARGET;)
+// 1. Alias Namespace (Sintaks Benar: global using ALIAS = TARGET;)
 global using AumoFinance = AumoBackend;
 global using AumoFinanceModels = AumoBackend.Core;
 global using AumoFinanceServices = AumoBackend.Core;
 global using AumoFinanceControllers = AumoBackend.Controllers;
 
 // 2. Direct Namespace Imports
-global using AumoBackend.Controllers;
 global using AumoBackend.Core;
+global using AumoBackend.Controllers;
