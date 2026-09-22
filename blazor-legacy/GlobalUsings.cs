43cd127a1b0327590766fc73d84138a2e9028efd
Obscuron <ndopoer@gmail.com>
2026-09-22 11:16:53 +0700

    Create GlobalUsings.cs

---
+++ blazor-legacy/GlobalUsings.cs
@@ -0,0 +1,10 @@
+// 1. Mapping Alias Namespace
+global using AumoFinance = AumoBackend;
+global using AumoFinance.Controllers = AumoBackend.Controllers;
+global using AumoFinance.Models = AumoBackend.Core;
+global using AumoFinance.Services = AumoBackend.Core;
+global using AumoFinance.Migrations = AumoBackend.Core;
+
+// 2. Import langsung namespace baru
+global using AumoBackend.Controllers;
+global using AumoBackend.Core;
