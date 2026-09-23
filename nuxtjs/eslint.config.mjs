97eede8ad23436ee4930a2096894dbd247beadb9
Obscuron <ndopoer@gmail.com>
2026-09-23 15:34:16 +0700

    up

---
+++ nuxtjs/eslint.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+import betterTailwindcss from 'eslint-plugin-better-tailwindcss'
+import { getDefaultAttributes } from 'eslint-plugin-better-tailwindcss/api/defaults'
+
+export default withNuxt(
+  {
+    rules: {
+      'vue/no-multiple-template-root': 'off',
+      'vue/max-attributes-per-line': ['error', { singleline: 3 }]
+    }
+  },
+  betterTailwindcss.configs['correctness-error'],
+  {
+    settings: {
+      'better-tailwindcss': {
+        entryPoint: 'app/assets/css/main.css',
+        attributes: [
+          ...getDefaultAttributes(),
+          ['^v-bind:ui$', [{ match: 'objectValues' }]]
+        ]
+      }
+    }
+  }
+)
