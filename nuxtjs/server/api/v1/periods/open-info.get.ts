8dcaecadc6fd4d9bdbf66e1bb9afec5348b020a4
Obscuron <ndopoer@gmail.com>
2026-09-26 15:34:10 +0700

    Add open-info API endpoint for account information

---
+++ nuxtjs/server/api/v1/periods/open-info.get.ts
@@ -0,0 +1,7 @@
+export default defineEventHandler(() => {
+  return {
+    hasExistingPermanentAccounts: false,
+    availableCashAndBankAccounts: [],
+    availableRetainedEarningsAccounts: []
+  }
+})
