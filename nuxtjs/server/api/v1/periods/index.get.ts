bb11bb271e575f0c55e247b4f331130b16cacaa4
Obscuron <ndopoer@gmail.com>
2026-09-26 15:44:03 +0700

    Refactor periods endpoint to improve readability

--- nuxtjs/server/api/v1/periods/index.get.ts
+++ nuxtjs/server/api/v1/periods/index.get.ts
@@ -1,11 +1,7 @@
-import { prisma } from '~~/server/utils/prisma'
-import { getUserId } from '~~/server/utils/auth'
-
 export default defineEventHandler(async (event) => {
-  const userId = await getUserId(event)
-  const periods = await prisma.periods.findMany({
+  const userId = await getUserId(event) // auto-import dari server/utils/auth.ts
+  return await prisma.periods.findMany({
     where: { UserId: userId },
     orderBy: { StartDate: 'desc' }
   })
-  return periods
 })
