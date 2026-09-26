a21e0ce928b31e3515b9d291d66f573fe37346c8
Obscuron <ndopoer@gmail.com>
2026-09-26 15:19:39 +0700

    Refactor AppSidebar links and onSelect handlers

--- nuxtjs/app/components/AppSidebar.vue
+++ nuxtjs/app/components/AppSidebar.vue
@@ -3,10 +3,18 @@ import type { NavigationMenuItem } from '@nuxt/ui'
 
 const open = defineModel<boolean>('open', { default: false })
 
-const links = computed(() => [[{
-  label: 'Home',
+const links = computed(() => [[
+{
+    label: 'Home',
   icon: 'i-lucide-house',
-  to: '/home', // Mengarah ke landing page member
+  to: '/home',
+  onSelect: () => {
+    open.value = false
+  }
+},{
+  label: 'Dashboard',
+  icon: 'i-lucide-layout-dashboard',
+  to: '/dashboard',
   onSelect: () => {
     open.value = false
   }
@@ -35,27 +43,19 @@ const links = computed(() => [[{
     label: 'General',
     to: '/settings',
     exact: true,
-    onSelect: () => {
-      open.value = false
-    }
+    onSelect: () => { open.value = false }
   }, {
     label: 'Members',
     to: '/settings/members',
-    onSelect: () => {
-      open.value = false
-    }
+    onSelect: () => { open.value = false }
   }, {
     label: 'Notifications',
     to: '/settings/notifications',
-    onSelect: () => {
-      open.value = false
-    }
+    onSelect: () => { open.value = false }
   }, {
     label: 'Security',
     to: '/settings/security',
-    onSelect: () => {
-      open.value = false
-    }
+    onSelect: () => { open.value = false }
   }]
 }], [{
   label: 'Feedback',
@@ -69,45 +69,5 @@ const links = computed(() => [[{
   target: '_blank'
 }]] satisfies NavigationMenuItem[][])
 
-// Expose data links agar bisa dibaca oleh parent jika dibutuhkan
 defineExpose({ links })
 </script>
-
-<template>
-  <UDashboardSidebar
-    id="default"
-    v-model:open="open"
-    collapsible
-    resizable
-    class="bg-elevated/25"
-    :ui="{ footer: 'lg:border-t lg:border-default' }"
-  >
-    <template #header="{ collapsed }">
-      <TeamsMenu :collapsed="collapsed" />
-    </template>
-
-    <template #default="{ collapsed }">
-      <UDashboardSearchButton :collapsed="collapsed" class="bg-transparent ring-default" />
-
-      <UNavigationMenu
-        :collapsed="collapsed"
-        :items="links[0]"
-        orientation="vertical"
-        tooltip
-        popover
-      />
-
-      <UNavigationMenu
-        :collapsed="collapsed"
-        :items="links[1]"
-        orientation="vertical"
-        tooltip
-        class="mt-auto"
-      />
-    </template>
-
-    <template #footer="{ collapsed }">
-      <UserMenu :collapsed="collapsed" />
-    </template>
-  </UDashboardSidebar>
-</template>
