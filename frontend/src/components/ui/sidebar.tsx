7820306d9f09b489e936d38b804359afd572b640
Obscuron <ndopoer@gmail.com>
2026-09-26 13:54:22 +0700

    Remove file extensions from import statements

--- frontend/src/components/ui/sidebar.tsx
+++ frontend/src/components/ui/sidebar.tsx
@@ -6,24 +6,24 @@ import { cn } from "cn";
 import { PanelLeftIcon } from "lucide-react";
 import { Slot } from "radix-ui";
 
-import { useIsMobile } from "#/hooks/use-mobile.ts";
-import { Button } from "#/components/ui/button.tsx";
-import { Input } from "#/components/ui/input.tsx";
-import { Separator } from "#/components/ui/separator.tsx";
+import { useIsMobile } from "#/hooks/use-mobile";
+import { Button } from "#/components/ui/button";
+import { Input } from "#/components/ui/input";
+import { Separator } from "#/components/ui/separator";
 import {
   Sheet,
   SheetContent,
   SheetDescription,
   SheetHeader,
   SheetTitle,
-} from "#/components/ui/sheet.tsx";
-import { Skeleton } from "#/components/ui/skeleton.tsx";
+} from "#/components/ui/sheet";
+import { Skeleton } from "#/components/ui/skeleton";
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "#/components/ui/tooltip.tsx";
+} from "#/components/ui/tooltip";
 
 const SIDEBAR_COOKIE_NAME = "sidebar_state";
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
