dd8a90adfbadb944064312935ffe9f412acb002d
Obscuron <ndopoer@gmail.com>
2026-09-26 15:44:32 +0700

    Update selected period for user in transaction

--- nuxtjs/server/api/v1/periods/[id]/select.post.ts
+++ nuxtjs/server/api/v1/periods/[id]/select.post.ts
@@ -1,6 +1,3 @@
-import { prisma } from '~~/server/utils/prisma'
-import { getUserId } from '~~/server/utils/auth'
-
 export default defineEventHandler(async (event) => {
   const userId = await getUserId(event)
   const id = Number(getRouterParam(event, 'id'))
