044169fc357a5e21aef281223bafabe7a10b5b32
Obscuron <ndopoer@gmail.com>
2026-09-26 15:32:38 +0700

    Add API endpoint to fetch user periods

---
+++ nuxtjs/server/api/v1/periods/index.get.ts
@@ -0,0 +1,12 @@
+import prisma from '~/server/utils/prisma'
+import { getUserId } from '~/server/utils/auth'
+
+export default defineEventHandler(async (event) => {
+  const userId = await getUserId(event)
+  
+  const periods = await prisma.periods.findMany({
+    where: { UserId: userId },
+    orderBy: { StartDate: 'desc' }
+  })
+  return periods
+})
