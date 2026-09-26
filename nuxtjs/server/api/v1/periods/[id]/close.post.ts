3127e32c8a3b5ad7fbd6de65f8d0e7801b04e4ac
Obscuron <ndopoer@gmail.com>
2026-09-26 15:44:45 +0700

    Refactor close post handler for periods

--- nuxtjs/server/api/v1/periods/[id]/close.post.ts
+++ nuxtjs/server/api/v1/periods/[id]/close.post.ts
@@ -1,15 +1,6 @@
-import prisma from '~/server/utils/prisma'
-import { getUserId } from '~/server/utils/auth'
-
 export default defineEventHandler(async (event) => {
   const userId = await getUserId(event)
   const id = Number(getRouterParam(event, 'id'))
-
-  const closed = await prisma.periods.updateMany({
-    where: { Id: id, UserId: userId },
-    data: { IsClosed: true, IsSelected: false }
-  })
-
-  if (closed.count === 0) throw createError({ statusCode: 404, message: 'Period not found' })
+  await prisma.periods.updateMany({ where: { Id: id, UserId: userId }, data: { IsClosed: true, IsSelected: false } })
   return { success: true }
 })
